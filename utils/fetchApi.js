f59eb4a967e399281f74f4cc4217c960a6bd0955
VladyslavMazurets <vladmazurec@gmail.com>
2022-05-28 12:21:30 +0200

    Finished

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,8 +5,8 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
-      'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+      'X-RapidAPI-Key': '7a0852ed29mshdfa55b45c270b7bp14ec01jsn01afae0b4e8c'
     },
   });
     
